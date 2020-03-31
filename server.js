ef227c221d705186aa3f06e6bb31481da2aa1641
cornedh <hello@cornedenhollander.nl>
2020-03-31 11:41:50 +0200

    add dynamic GET route

--- server.js
+++ server.js
@@ -13,5 +13,14 @@ app.get('/users',function(req,res){
 		users: mockUserData
 	})
 })
+// colons are used as variables that be viewed in the params
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
 
 app.listen(8000,function(){console.log('server is listening')})
